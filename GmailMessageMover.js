7bf2357aa9f61e273860989a834afca45f9dc4d9
Sean Larabell <smlarabell@gmail.com>
2025-08-07 13:15:56 -0400

    Update GmailMessageMover.js

--- GmailMessageMover.js
+++ GmailMessageMover.js
@@ -9,15 +9,10 @@ function MoveEmailsBySearch() {
     cutoffDate.setDate(cutoffDate.getDate() - daysOld);
     var cutoffDateStr = Utilities.formatDate(cutoffDate, Session.getScriptTimeZone(), "yyyy/MM/dd");
     var batchSize = 50;
-    var threads;
     for (var i = 0; i < categories.length; i++) {
         // Search for threads in category, not starred, older than cutoffDate
-        var query = "".concat(categories[i], " -is:starred before:").concat(cutoffDateStr);
-        do {
-            threads = GmailApp.search(query, 0, batchSize);
-            for (var j = 0; j < threads.length; j++) {
-                threads[j].moveToTrash();
-            }
-        } while (threads.length === batchSize);
+        var query = categories[i] + " -is:starred before:" + cutoffDateStr;
+        var threads_1 = GmailApp.search(query, 0, batchSize);
+        GmailApp.moveThreadsToTrash(threads_1);
     }
 }
